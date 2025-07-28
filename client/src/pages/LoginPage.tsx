95735c1c33d4fdb9db6282adaf2240e2c6172efc
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-07-27 20:14:34 -0500

    add login redirect to profile after login or if user already logged in

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,21 +1,30 @@
 import { LoginForm } from '@/features/auth';
 import { useAuth } from '@/features/auth/hooks/useAuth';
 import { useAuthStore } from '@/features/auth/stores/';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const { login } = useAuth();
   const token = useAuthStore(state => state.token);
   const user = useAuthStore(state => state.user);
+  const navigate = useNavigate();
   const { mutateAsync: loginMutate, isError, isPending, isSuccess } = login;
 
   let displayComponent: React.ReactNode = <LoginForm apiCall={loginMutate} />;
 
-  if (isError) {
-    console.error('Login failed');
-  }
+  useEffect(() => {
+    if (token && user) {
+      navigate('/profile');
+    }
+  }, [token, user, navigate]);
 
   if (isSuccess) {
-    console.log('Login successful', { token, user });
+    console.log('Login successful', user, token);
+  }
+
+  if (isError) {
+    console.error('Login failed');
   }
 
   if (isPending) {
