70c8027d09dbfb910d4ad1db61627a2bf9cd9784
jcloud <cloudsprogramming@gmail.com>
2025-07-28 20:47:00 -0700

    feat(services): add get all services

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,16 @@
-import { Router } from "express";
-import authRoutes from "./auth";
-import usersRoutes from "./users";
+import { Router } from 'express';
+import authRoutes from './auth';
+import usersRoutes from './users';
+import servicesRoutes from './services';
 // const postsRoutes = require("./posts");
-import { isAuth } from "../middleware/authMiddleware";
+import { isAuth } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.use("/auth", authRoutes);
+router.use('/auth', authRoutes);
 // router.use("/users", isAuth, usersRoutes);
-router.use("/users", usersRoutes);
+router.use('/users', usersRoutes);
 // router.use("/posts", isAuth, postsRoutes);
+router.use('/services', servicesRoutes);
 
 export default router;
