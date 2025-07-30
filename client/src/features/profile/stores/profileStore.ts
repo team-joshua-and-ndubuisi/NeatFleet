d0b2fdd98949a7e89c1acbc4fea87d441be35bf9
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-07-30 13:22:10 -0500

    fix store interface

--- client/src/features/profile/stores/profileStore.ts
+++ client/src/features/profile/stores/profileStore.ts
@@ -1,11 +1,11 @@
 import { UserProfileT } from '@/features/profile/';
 import { createLocalPersistStore } from '@/lib/utils';
-interface AuthStore {
+interface ProfileStore {
   profile: UserProfileT;
 }
 
 // good one
-export const useAuthStore = createLocalPersistStore<AuthStore>(
+export const useAuthStore = createLocalPersistStore<ProfileStore>(
   set => ({
     profile: {} as UserProfileT,
     setProfile: (profile: UserProfileT) => set(() => ({ profile })),
