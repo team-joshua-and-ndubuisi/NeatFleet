499bad1e0bb852e2d09952eac6d84c8cafb0d4cc
jcloud <cloudsprogramming@gmail.com>
2025-07-30 16:09:12 -0700

    chores(availability): set up availability controller

--- server/src/controllers/availabilityController.ts
+++ server/src/controllers/availabilityController.ts
@@ -1,7 +1,10 @@
 // server/src/controllers/availabilityController.ts
 import asyncHandler from 'express-async-handler';
 import { Request, Response, NextFunction } from 'express';
-import { getUpcomingAvailableDatesByServiceId } from '../services/availabilityService';
+import {
+  getUpcomingAvailableDatesByServiceId,
+  getAvailableTimeBlocks,
+} from '../services/availabilityService';
 import { logger } from '../config/logger';
 
 // @desc    Get upcoming availability dates for a specific service
@@ -18,4 +21,19 @@ const getServiceDateAvailability = asyncHandler(
   }
 );
 
-export { getServiceDateAvailability };
+// @desc
+// @route
+// @access  Public
+const getServiceTimeBlockAvailability = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { service_id } = req.params;
+    const { date } = req.query;
+    const serviceId = service_id;
+
+    logger.info(`Fetching availability time block for service ${serviceId}`);
+    const timeBlocks = await getAvailableTimeBlocks(serviceId);
+    res.status(200).json({ time_blocks: timeBlocks });
+  }
+);
+
+export { getServiceDateAvailability, getServiceTimeBlockAvailability };
