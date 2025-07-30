ee20bb12ff738244e75977f8319e4a65c49ae54e
Max Campos <mcampos2a@gmail.com>
2025-07-30 11:20:27 -0500

    refactor types for BookServiceForm page
    Co-authored-by: Andrew Smith <meuzishun@users.noreply.github.com>

--- client/src/features/bookService/types/index.ts
+++ client/src/features/bookService/types/index.ts
@@ -1,2 +1,4 @@
 export type { ServiceFormData } from './ServiceFormDataType';
 export type { ServiceFormState } from './ServiceFormState';
+export type { FormData } from './FormData';
+export type { TimeSlot } from './TimeSlot';
