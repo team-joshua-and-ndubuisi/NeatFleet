1a10a2bfb05c4ab9ab121745561eb3386ca499d5
Max Campos <mcampos2a@gmail.com>
2025-07-30 14:08:52 -0500

    Adjust margin on booking services page wrapper, update nav bar services button, format routes.

--- client/src/pages/ServiceCatalogPage.tsx
+++ client/src/pages/ServiceCatalogPage.tsx
@@ -3,8 +3,7 @@ import { BookServiceForm } from '@/features/bookService';
 
 const ServiceCatalogPage: React.FC = () => {
   return (
-    <div className='mt-16 px-8'>
-      <h4 className='text-3xl mb-4'>Service Catalog Page</h4>
+    <div className='mt-16 mb-16 px-8'>
       <BookServiceForm />
     </div>
   );
