b68603c3a21d498d1d212be3f192839a08cff427
Max Campos <mcampos2a@gmail.com>
2025-07-31 09:34:36 -0500

    Create TimeSlot, Address, and Payment data types. Add them to exports.
    Co-authored-by: Andrew Smith <meuzishun@users.noreply.github.com>

--- client/src/features/bookService/types/FormData.ts
+++ client/src/features/bookService/types/FormData.ts
@@ -1,10 +1,22 @@
 import { TimeSlot } from '@/features/bookService';
 import { Service } from '@/features/services';
 import { Technician } from '@/features/technicians';
+import { Address, City, State, Apartment, Zipcode } from '@/features/bookService';
+import { CardName, CardNumber, CVC, Expiry, Zip } from '@/features/bookService';
 
 export interface FormData {
   service: Service | null;
   date: Date | null;
   timeSlot: TimeSlot | '';
   technician: Technician | null;
+  address: Address | null;
+  apartment: Apartment | null;
+  city: City | null;
+  state: State | null;
+  zipcode: Zipcode | null;
+  cardName: CardName | null;
+  cardNumber: CardNumber | null;
+  expiry: Expiry | null;
+  cvc: CVC | null;
+  zip: Zip | null;
 }
---
+++ client/src/features/bookService/types/Payment.ts
@@ -0,0 +1,5 @@
+export type CardName = string | null;
+export type CardNumber = string | null;
+export type CVC = string | null;
+export type Expiry = string | null;
+export type Zip = string | null;
