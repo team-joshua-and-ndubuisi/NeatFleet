5e3c82e9a21d627478c6166838f27e2a86c646a2
jcloud <cloudsprogramming@gmail.com>
2025-07-30 20:01:28 -0700

    feat(availabilites): introduce query that allows retreival of time blocks for a date

--- server/src/services/availabilityService.ts
+++ server/src/services/availabilityService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../config/prisma';
 import { addDays, format } from 'date-fns';
+import { TimeBlock } from '../../generated/prisma';
 
 async function getUpcomingAvailableDatesByServiceId(
   serviceId: string
@@ -28,4 +29,35 @@ async function getUpcomingAvailableDatesByServiceId(
   }
 }
 
-export { getUpcomingAvailableDatesByServiceId };
+async function getAvailableTimeBlocks(serviceId: string, date: string) {
+  const result: { time_block: string }[] = await prismaClient.$queryRaw<
+    { time_block: TimeBlock }[]
+  >`
+    SELECT DISTINCT ta.time_block
+    FROM technician_availabilities ta
+    JOIN technicians_services ts ON ta.technician_id = ts.technician_id
+    WHERE ts.service_id = ${serviceId}::uuid
+      AND ta.available_date = ${date}
+  `;
+
+  // list all possible time blocks (matches enum)
+  const allBlocks: TimeBlock[] = ['morning', 'afternoon', 'evening'];
+
+  const resultBlocks = result.map(row => row.time_block);
+
+  // build this with default value as false
+  const availabilityMap: Record<TimeBlock, boolean> = {
+    morning: false,
+    afternoon: false,
+    evening: false,
+  };
+
+  // update a specific timeblock property to true if it exist in resultBlock
+  for (const block of allBlocks) {
+    availabilityMap[block] = resultBlocks.includes(block);
+  }
+
+  return availabilityMap;
+}
+
+export { getUpcomingAvailableDatesByServiceId, getAvailableTimeBlocks };
