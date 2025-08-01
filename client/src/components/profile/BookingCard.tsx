6edc1159ec08a82ec87c31b51ed969404ec79c87
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-08-01 11:08:18 -0500

    fix 4th column values for demo (#41)
    * fix 4th column values for demo
    
    * add styling for demo
    
    * demo stylings
    
    * fix rating centering
    
    * chore date format
    
    * break apart bookings accordians and add title prop to booking snippet
    
    * fix user data bookings
    
    * fix small styling changes
    
    * styling

--- client/src/components/profile/BookingCard.tsx
+++ client/src/components/profile/BookingCard.tsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-interface BookingProps{
-    name: string,
-    status: string,
-    date: number|string,
-    details: string,
-    rating: number
+interface BookingProps {
+  name: string;
+  status: string;
+  date: number | string;
+  details: string;
+  rating: number;
+  id: string;
 }
 
-   const BookingCard: React.FC<BookingProps>=({name,status,date,details,rating})=>{
-    return (
-      <div className="parent-container">
-       <div className="flex py-10">
-            <div className="border border-black py-3 w-1/5 text-wrap text-center lg:text-xl md:text-xl text-sm  border-round-5x rounded-tl-lg rounded-bl-lg">
-
-                 <span> Name: {name}</span>
-            </div>
-            <div className="border border-black py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x">
-                <span>{status}</span>
-            </div>
-            <div className="border border-black py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x">
-                <span>Service Date: {date}</span>
-            </div>
-            <div className="border border-black py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x">
-
-                <span>{details}</span>
-            </div>
-             <div className="flex flex-row border border-black py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x rounded-br-lg rounded-tr-lg text-center">
-                <span>Rating: {rating}</span>
-            </div>
-       </div>
+const BookingCard: React.FC<BookingProps> = ({ name, status, date, details, rating, id }) => {
+  return (
+    <div className=' shadow-md bg-white rounded-lg '>
+      <div className='flex'>
+        <div className='border  py-3 w-1/5 text-wrap text-center lg:text-xl md:text-xl text-sm  border-round-5x rounded-tl-lg rounded-bl-lg'>
+          <span> Name: {name}</span>
+        </div>
+        <div className='border  py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x'>
+          <Link to={`/bookings/${id}`}>
+            <span
+              className={cn(
+                'hover:pointer-coarse text-accent-green underline shadow-2xs',
+                {
+                  'text-accent-red': status === 'cancelled',
+                },
+                {
+                  'text-primary': status === 'upcoming',
+                }
+              )}
+            >
+              {status}
+            </span>
+          </Link>
+        </div>
+        <div className='border  py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x'>
+          <span>Service Date: {date}</span>
+        </div>
+        <div className='border  py-3 text-wrap text-center w-1/5 lg:text-xl md:text-xl text-sm border-round-5x'>
+          <span>{details}</span>
+        </div>
+        <div className='border  py-3 text-wrap w-1/5 lg:text-xl md:text-xl text-sm border-round-5x rounded-br-lg rounded-tr-lg text-center'>
+          <span>Rating: {rating}</span>
+        </div>
       </div>
-    )
-  }
-  export default BookingCard
+    </div>
+  );
+};
+export default BookingCard;
