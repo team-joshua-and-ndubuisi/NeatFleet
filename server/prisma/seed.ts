cc0befbbc27e8af190a0994b7fdb448dc7e1ae19
227Faddi <falukhouma27@gmail.com>
2025-08-01 18:08:53 -0400

    fix: seed data imports

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../src/config/prisma';
 
 async function main() {
   // Clean up existing data
