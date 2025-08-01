add824b87867b0f6297c12837632ec28b4a77fd0
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2025-08-01 11:08:18 -0500

    fix 4th column values for demo (#41)
    * fix 4th column values for demo
    
    * add styling for demo
    
    * demo stylings
    
    * fix rating centering
    
    * chore date format
    
    * break apart bookings accordians and add title prop to booking snippet
    
    * fix user data bookings
    
    * fix small styling changes
    
    * styling

--- client/src/components/profile/BookingSnippet.tsx
+++ client/src/components/profile/BookingSnippet.tsx
@@ -1,48 +1,28 @@
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
-  import {FunctionComponent, PropsWithChildren} from 'react';
-
-
-   const BookingSnippets: FunctionComponent<PropsWithChildren> =({children})=> {
-    return (
-      <div>
-          <div className="w-full py-10">
-            <div>
-            <Accordion type="single" collapsible>
-              <AccordionItem    className="py-5" value="item-1">
-                <AccordionTrigger className="bg-stone-300 border-3 border border-black ">
-                  <h3 className="px-3 text-3xl" >
-                    Scheduled Bookings
-                  </h3>
-                </AccordionTrigger>
-                <AccordionContent className="flex flex-col justify-center">
-                 {children}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-            </div>
-            <div>
-            <Accordion type="single" collapsible>
-              <AccordionItem  className="py-5" value="item-1">
-                <AccordionTrigger className="bg-stone-300 border-3 border border-black">
-                  <h3 className="px-3 text-3xl" >
-                    Past Bookings
-                  </h3>
-                </AccordionTrigger>
-                <AccordionContent className="flex flex-col justify-center">
-                  {/* Child is map of booking car */}
-                    {children}
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-            </div>
-          </div>
+const BookingSnippets = ({ children, title }: { children: React.ReactNode; title: string }) => {
+  return (
+    <div>
+      <div className='w-full py-10'>
+        <div>
+          <Accordion type='single' collapsible>
+            <AccordionItem className='py-5' value='item-1'>
+              <AccordionTrigger className='bg-stone-300 border border-slate-500 rounded-lg shadow-md'>
+                <h3 className='px-3 text-3xl'>{title}</h3>
+              </AccordionTrigger>
+              <AccordionContent className='flex flex-col justify-center gap-3'>
+                {children}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
-    )
-  }
-  export default BookingSnippets
+    </div>
+  );
+};
+export default BookingSnippets;
