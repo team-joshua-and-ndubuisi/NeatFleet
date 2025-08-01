c088010aff4355e21497efcdc1250d0223cb59c2
Max Campos <mcampos2a@gmail.com>
2025-08-01 11:12:06 -0500

    Change HomeScreen component name to avoid confusion. Add image to landing page. Add images to public folder.

--- client/src/data/homeInfoCards.ts
+++ client/src/data/homeInfoCards.ts
@@ -1,32 +1,21 @@
 export interface InfoCardData {
-    title: string;
-    lines: string[];
+  title: string;
+  lines: string[];
 }
 
 const homeInfoCards: InfoCardData[] = [
-    {
-        title: "Serivce Hours",
-        lines: [
-            "Mon - Fri: 9:00 AM - 5:00 PM",
-            "Sat: 10:00 AM - 2:00 PM",
-            "Sun: Closed",
-        ]
-    },
-    {
-        title: "Business Address",
-        lines: [
-            "1234 Fleet Avenue",
-            "Suite 567",
-            "Dallas, TX 12345"
-        ]
-    },
-    {
-        title: "Contact Information",
-        lines: [
-            "Email: contact@neatfleet.com",
-            "Phone: (123) 456-7890",
-        ]
-    }
-]
+  {
+    title: 'Serivce Hours',
+    lines: ['Mon - Fri: 8:00 AM - 8:00 PM', 'Sat: 8:00 AM - 4:00 PM', 'Sun: Closed'],
+  },
+  {
+    title: 'Business Address',
+    lines: ['73192 Fleet Avenue', 'Suite 117', 'Dallas, TX 76201'],
+  },
+  {
+    title: 'Contact Information',
+    lines: ['Email: contact@neatfleet.com', 'Phone: (972) 456-7890'],
+  },
+];
 
-export default homeInfoCards
+export default homeInfoCards;
