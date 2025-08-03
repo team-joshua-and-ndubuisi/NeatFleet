6398cba57ac3b34dd473c539a577545b945f38d1
227Faddi <falukhouma27@gmail.com>
2025-08-03 11:59:33 -0400

    feat: integrate stripe payments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from '@/routes';
+import { CheckoutProvider } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { axiosInstance } from './api';
+import axios from 'axios';
 
 const queryClient = new QueryClient();
+const stripePromise = await loadStripe(import.meta.env.VITE_STRIPE_SECRET as string);
 
 function App() {
+  const fetchClientSecret = async () => {
+    // TODO: Replace with axiosInstance once the backend is integrated.
+    // const response = await axiosInstance.post('/bookings/create-checkout-session');
+
+    const response = await axios.post('http://localhost:3000/api/bookings/create-checkout-session');
+    const secret = response.data.checkoutSessionClientSecret;
+    return secret;
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
-      <AppRoutes />
+      <CheckoutProvider stripe={stripePromise} options={{ fetchClientSecret }}>
+        <AppRoutes />
+      </CheckoutProvider>
     </QueryClientProvider>
   );
 }
