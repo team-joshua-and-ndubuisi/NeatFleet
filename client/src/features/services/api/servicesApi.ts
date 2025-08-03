32d62907d4c6ef41c80e53db5aeceba895972854
Andrew Smith <meuzishun@gmail.com>
2025-08-03 16:10:30 -0700

    Adjust return value for servicesApi so less refactoring in BookServiceForm

--- client/src/features/services/api/servicesApi.ts
+++ client/src/features/services/api/servicesApi.ts
@@ -1,9 +1,9 @@
 import { axiosInstance } from '@/api';
-import type { ServicesData } from '@/features/services';
+import type { Service } from '@/features/services';
 
 const url = '/services';
 
-export const fetchServices = async (): Promise<ServicesData> => {
+export const fetchServices = async (): Promise<Service[]> => {
   const response = await axiosInstance.get(url);
-  return response.data;
+  return response.data.services;
 };
