0a9150fceae95498a94259bec59ef20b31abc8fa
jcloud <cloudsprogramming@gmail.com>
2025-08-04 13:46:12 -0700

    chores(address): add routes for adddresses related endpoints

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -17,4 +17,9 @@ router.get('/search', searchUsers);
 router.get('/', getUsers);
 router.get('/:userId', userIdValidator, checkValidations, getSingleUser);
 
+router.post('/addresses', createAddress); // POST /api/users/addresses
+router.get('/addresses', getUserAddresses); // GET /api/users/addresses
+router.patch('/addresses/:address_id', updateAddress); // PATCH /api/users/addresses/:address_id
+router.delete('/addresses/:address_id', deleteAddress); // DELETE /api/users/addresses/:address_id
+
 export default router;
