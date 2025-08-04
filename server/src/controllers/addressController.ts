ff456cff8a79b1aac1bbd9d8a8ee565e97fd4f73
jcloud <cloudsprogramming@gmail.com>
2025-08-04 13:50:29 -0700

    chores(address): add controller

---
+++ server/src/controllers/addressController.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import { logger } from '../config/logger';
+import prisma from '../config/prisma';
+import { ExtendedErrorT } from '../types/error';
+
+export {};
