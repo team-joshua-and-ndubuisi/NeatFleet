7b5c01ab4fcd92042a073f10260fda6792b7fc7b
Andrew Smith <meuzishun@gmail.com>
2025-08-03 17:33:50 -0700

    Adjust Technician type

--- client/src/features/technicians/types/Technician.ts
+++ client/src/features/technicians/types/Technician.ts
@@ -1,12 +1,7 @@
+import { User } from '@/features/users';
+
 export interface Technician {
   id: string;
-  user_id: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone: string;
-  password: string;
-  created_at: string;
-  is_active: boolean;
   current_rating: number;
+  user: User;
 }
