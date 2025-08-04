0212e16b76a1a377ad1961b20c9d444df62a5c0f
Andrew Smith <meuzishun@gmail.com>
2025-08-03 17:35:03 -0700

    Adjust availableTechniciansApi

--- client/src/features/bookService/api/availableTechniciansApi.ts
+++ client/src/features/bookService/api/availableTechniciansApi.ts
@@ -1,28 +1,25 @@
 import { axiosInstance } from '@/api';
-import type { AvailableTechnician } from '@/features/bookService';
+import type { Technician } from '@/features/technicians';
 
-const url = '/availableTechnicians';
+const url = '/availabilities/service';
 
 export const fetchAvailableTechnicians = async (
   serviceId: string | null | undefined,
   date: Date | null | undefined,
   time: string | null | undefined
-): Promise<AvailableTechnician[]> => {
+): Promise<Technician[]> => {
   if (!serviceId || !date || !time) {
     throw new Error('Service ID, date, and time are required');
   }
 
-  const normalizedDate = new Date(
-    Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
-  );
+  const formattedDate = date.toISOString().split('T')[0];
 
-  const formattedDate = normalizedDate.toISOString();
+  const response = await axiosInstance.get<{ technicians: Technician[] }>(`${url}/${serviceId}`, {
+    params: {
+      date: formattedDate,
+      time_block: time,
+    },
+  });
 
-  const response = await axiosInstance.get(
-    `${url}?serviceId=${serviceId}&date=${formattedDate}&time=${time}`
-  );
-
-  console.log(`${url}?serviceId=${serviceId}&date=${formattedDate}&time=${time}`);
-
-  return response.data;
+  return response.data.technicians;
 };
