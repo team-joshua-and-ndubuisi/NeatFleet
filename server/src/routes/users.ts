442141caf74fc039a2a894e091add6a2d7aeaf69
jcloud <cloudsprogramming@gmail.com>
2025-08-04 21:18:55 -0700

    fix(users route): moved static route (/addresses) to the top, to avoid errors in dynmic route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -14,19 +14,19 @@ import {
 import {
   addAddress,
   // deleteAddress,
-  // getUserAddresses,
+  getUserAddresses,
   // updateAddress,
 } from '../controllers/addressController';
 
 const router = Router();
 
+router.post('/addresses', addressValidator, checkValidations, addAddress); // POST /api/users/addresses
+router.get('/addresses', getUserAddresses); // GET /api/users/addresses
+// router.patch('/addresses/:address_id', updateAddress); // PATCH /api/users/addresses/:address_id
+// router.delete('/addresses/:address_id', deleteAddress); // DELETE /api/users/addresses/:address_id
+
 router.get('/search', searchUsers);
 router.get('/', getUsers);
 router.get('/:userId', userIdValidator, checkValidations, getSingleUser);
-
-router.post('/addresses', addressValidator, checkValidations, addAddress); // POST /api/users/addresses
-// router.get('/addresses', getUserAddresses); // GET /api/users/addresses
-// router.patch('/addresses/:address_id', updateAddress); // PATCH /api/users/addresses/:address_id
-// router.delete('/addresses/:address_id', deleteAddress); // DELETE /api/users/addresses/:address_id
 
 export default router;
