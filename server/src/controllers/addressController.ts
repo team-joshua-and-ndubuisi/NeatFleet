a1fc63ace210523a2bde0f3aace91cf5683fcbf9
jcloud <cloudsprogramming@gmail.com>
2025-08-04 20:26:27 -0700

    feat(address): endpoint works for createAddress

--- server/src/controllers/addressController.ts
+++ server/src/controllers/addressController.ts
@@ -2,6 +2,31 @@ import { NextFunction, Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import { logger } from '../config/logger';
 import prisma from '../config/prisma';
-import { ExtendedErrorT } from '../types/error';
+import { AppError } from '../types/error';
+import { User as UserType } from '../../generated/prisma';
+import { createAddress } from '../services/addressService';
 
-export {};
+// Create Address
+const addAddress = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { street, city, state, zip, latitude, longitude } = req.body;
+    const userId = (req.user as UserType).id;
+
+    if (!userId) throw new AppError('Unauthorized', 401);
+
+    const address = await createAddress({
+      userId,
+      street,
+      city,
+      state,
+      zip,
+      latitude,
+      longitude,
+    });
+
+    logger.info(`Address created for user ${userId}`);
+    res.status(201).json(address);
+  }
+);
+// getUserAddresses, updateAddress, deleteAddress
+export { addAddress };
