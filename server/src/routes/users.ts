d38a1ccf9848160946f905653e958586fca37c7e
CloudsProgram <62721390+CloudsProgram@users.noreply.github.com>
2025-08-07 14:39:02 -0700

    TJN-32 create backend endpoints for user (#56)
    * feat(userService): add to check if a specific user id exist in db and also check to see if tech or not
    
    * chores(userService): add logger
    
    * chores(userService + authController): add logger and clean tempalte
    
    * chores(userService): adjust user info returned
    
    * chores(authController): establishing control flow for userProfile
    
    * feat(get user profile): introduce service query
    
    * chores(authController): comment out not functional yet method
    
    * chores(address): add routes for adddresses related endpoints
    
    * chores(address): add controller
    
    * chores(address): add address input validators
    
    * chores(error): add custom resuable AppError class
    
    * chores(error): introduce cleaner error throw
    
    * feat(address): endpoint works for createAddress
    
    * fix(users route): moved static route (/addresses) to the top, to avoid errors in dynmic route
    
    * chores(address): rename var name to getAddressesForUser  to avoid conflict with getUserAddresses
    
    * feat(get user addresses): added a functional end point
    
    * chores(clean up): delete un-neede comment
    
    * chores(edit address): estalish route + have stub in controller
    
    * feat(edit address): add functional  edit address endpoint
    
    * chores(address): add address latitude, longitude and isPrimary validators
    
    * fix(technician): adjust error message in isTechnician
    
    * chores(isAuth middleware): attach user's role to req.user for easy access
    
    * fix(profile) returned profile info to include service name and rid of details
    
    * chores(users): rid of unneeded query
    
    * feat(dateUtils): introduce amount of years (round down) on neat fleet
    
    * chores(type): add optional technicianId property for req.user which will be added via isAuth
    
    * feat(technicianServices): introduce get technician id
    
    * chores(isAuth): add optional req.technicianId, for quick access
    
    * feat(booking): add query that returns amount of completed bookings based on role
    
    * feat(userProfile): have profile return number of bookings + years with neat fleet
    
    * feat(technician profile): add query that gets all info associated with technician's profile
    
    * chores(auth profile): endpoint to retreive technician profile works now
    
    * chores(get tech / user profile): tuck away stats to userService + simplied userProfile
    
    * chores: clean up debug comments
    
    * chores(user profile): add email and phone number as part of the return obj for the endpoint
    
    * chores(technician profile) update end point to also return phone + email for technician
    
    * chores(years with neat fleet): added comments to specifiy what calc years does

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import {
   userIdValidator,
   checkValidations,
+  addressValidator,
 } from '../middleware/inputValidators';
 
 import {
@@ -10,8 +11,24 @@ import {
   getUsers,
   getSingleUser,
 } from '../controllers/usersController';
+import {
+  addAddress,
+  // deleteAddress,
+  getUserAddresses,
+  updateAddress,
+} from '../controllers/addressController';
 
 const router = Router();
+
+router.post('/addresses', addressValidator, checkValidations, addAddress); // POST /api/users/addresses
+router.get('/addresses', getUserAddresses); // GET /api/users/addresses
+router.patch(
+  '/addresses/:address_id',
+  addressValidator,
+  checkValidations,
+  updateAddress
+); // PATCH /api/users/addresses/:address_id
+// router.delete('/addresses/:address_id', deleteAddress); // DELETE /api/users/addresses/:address_id
 
 router.get('/search', searchUsers);
 router.get('/', getUsers);
--- server/src/services/addressService.ts
+++ server/src/services/addressService.ts
@@ -1,7 +1,8 @@
 import prismaClient from '../config/prisma'; // Ensure your db connection is set up correctly
 import { Address } from '../../generated/prisma';
+import { AppError } from '../types/error';
 
-const getUserAddresses = async (userId: string): Promise<Address[]> => {
+const getAddressesForUser = async (userId: string): Promise<Address[]> => {
   try {
     const addresses = await prismaClient.address.findMany({
       where: { user_id: userId },
@@ -53,4 +54,52 @@ const createAddress = async ({
   }
 };
 
-export { createAddress, getUserAddresses };
+type UpdateAddressInput = {
+  addressId: string;
+  userId: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  latitude: number | null;
+  longitude: number | null;
+  isPrimary?: boolean;
+};
+
+const updateUserAddress = async ({
+  addressId,
+  userId,
+  street,
+  city,
+  state,
+  zip,
+  latitude,
+  longitude,
+  isPrimary,
+}: UpdateAddressInput) => {
+  // Verify address ownership
+  const address = await prismaClient.address.findUnique({
+    where: { id: addressId },
+  });
+
+  if (!address || address.user_id !== userId) {
+    throw new AppError('Address not found or unauthorized', 404);
+  }
+
+  const returnAddress = await prismaClient.address.update({
+    where: { id: addressId },
+    data: {
+      street,
+      city,
+      state,
+      zip,
+      latitude,
+      longitude,
+      ...(isPrimary !== undefined && { isPrimary }),
+    },
+  });
+
+  return returnAddress;
+};
+
+export { createAddress, getAddressesForUser, updateUserAddress };
