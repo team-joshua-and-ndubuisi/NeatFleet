2fbe7ee57d3931c24e022b26ba9cd9eef084f380
Max Campos <mcampos2a@gmail.com>
2025-08-06 20:06:19 -0500

    delete old card payment data properties in zustand store and add persistence

--- client/src/features/bookService/stores/useServiceFormStore.ts
+++ client/src/features/bookService/stores/useServiceFormStore.ts
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { FormData, ServiceFormState } from '@/features/bookService';
 
-const initialFormState = {
+const initialFormState: FormData = {
   service: null,
   date: null,
   timeSlot: null,
@@ -11,37 +12,40 @@ const initialFormState = {
   city: null,
   state: null,
   zipcode: null,
-  cardName: null,
-  cardNumber: null,
-  cvc: null,
-  expiry: null,
-  zip: null,
 };
 
 const fieldOrder = ['service', 'date', 'timeSlot', 'technician'];
 
-export const useServiceFormStore = create<ServiceFormState>(set => ({
-  formData: initialFormState,
-  reset: () => set({ formData: initialFormState }),
-  setFormData: (update: Partial<FormData>) =>
-    set(state => {
-      const updatedField = Object.keys(update)[0] as keyof FormData;
-      const updatedFieldIndex = fieldOrder.indexOf(updatedField);
+export const useServiceFormStore = create<ServiceFormState>()(
+  persist(
+    set => ({
+      formData: initialFormState,
+      reset: () => set({ formData: initialFormState }),
+      setFormData: (update: Partial<FormData>) =>
+        set(state => {
+          const updatedField = Object.keys(update)[0] as keyof FormData;
+          const updatedFieldIndex = fieldOrder.indexOf(updatedField);
 
-      const nullifiedFields =
-        updatedFieldIndex !== -1
-          ? fieldOrder.slice(updatedFieldIndex + 1).reduce<Partial<FormData>>((acc, field) => {
-              acc[field as keyof FormData] = null;
-              return acc;
-            }, {})
-          : {};
+          const nullifiedFields =
+            updatedFieldIndex !== -1
+              ? fieldOrder.slice(updatedFieldIndex + 1).reduce<Partial<FormData>>((acc, field) => {
+                  acc[field as keyof FormData] = null;
+                  return acc;
+                }, {})
+              : {};
 
-      return {
-        formData: {
-          ...state.formData,
-          ...update,
-          ...nullifiedFields,
-        },
-      };
+          return {
+            formData: {
+              ...state.formData,
+              ...update,
+              ...nullifiedFields,
+            },
+          };
+        }),
     }),
-}));
+    {
+      name: 'service-form-storage', // key for localStorage
+      partialize: state => ({ formData: state.formData }), // only persist the formData
+    }
+  )
+);
