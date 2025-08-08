646c8fe3ddea1bd96bbcc14078be07725ee9abf7
jesus doza <jesus.doza@gmail.com>
2025-08-07 20:37:31 -0500

    fix useAddAddress client/src/features/profile/hooks/useAddAddress.ts

--- client/src/features/profile/hooks/useAddAddress.ts
+++ client/src/features/profile/hooks/useAddAddress.ts
@@ -2,8 +2,8 @@ import { useMutation } from '@tanstack/react-query';
 import { addAddress } from '@/features/profile/api/addressApi';
 import { AddressT } from '../types';
 
-export const useAddAddress = (userId: string) => {
+export const useAddAddress = (userToken: string) => {
   return useMutation({
-    mutationFn: (address: AddressT) => addAddress(userId, address),
+    mutationFn: (address: AddressT) => addAddress(userToken, address),
   });
 };
