e002087fc2173e57dd2734de372e64815a2605a5
Max Campos <mcampos2a@gmail.com>
2025-08-10 14:10:07 -0500

    create booking route and add booking controller method to handle Stripe intent and payment verification.

--- server/src/routes/bookings.ts
+++ server/src/routes/bookings.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 import {
   addBooking,
-  checkoutStripe,
+  createPaymentIntent,
+  verifyPaymentIntent,
   deleteBooking,
   getBooking,
   getBookings,
   updateBooking,
 } from '../controllers/bookingsController';
 const router = Router();
-
-router.post('/create-checkout-session', checkoutStripe);
 router.post('/', addBooking);
 router.get('/', getBookings);
 router.get('/:bookingId', getBooking);
 router.patch('/:bookingId', updateBooking);
 router.delete('/:bookingId', deleteBooking);
-
+router.post('/create-payment-intent', createPaymentIntent);
+router.get('/verify-payment-intent', verifyPaymentIntent);
 export default router;
