45fbcf07187a84170c5130ba9bee55887b0dc870
Andrew Smith <meuzishun@gmail.com>
2025-08-09 19:59:33 -0700

    Adjust comment in Booking component

--- client/src/features/bookings/components/Booking.tsx
+++ client/src/features/bookings/components/Booking.tsx
@@ -6,9 +6,9 @@ interface BookingProps {
 }
 
 const Booking: React.FC<BookingProps> = ({ booking }) => {
+  // TODO: Feel free to display and style whatever and however you want
   return (
     <div>
-      // TODO: Feel free to display and style whatever and however you want
       <p>id: {booking.id}</p>
       <p>user_id: {booking.user_id}</p>
       <p>technician_id: {booking.technician_id}</p>
