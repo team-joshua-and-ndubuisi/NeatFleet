da98fdecbcd587aa5b9e46c420b144d7dcec1422
Andrew Smith <meuzishun@gmail.com>
2025-08-11 16:03:16 -0400

    Create AdminLinks and TechnicianLinks

---
+++ client/src/features/profile/components/AdminLinks.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function AdminLinks() {
+  return (
+    <div className='flex flex-col items-center'>
+      <Link
+        to='/profile/manage-technicians'
+        className='text-md font-semibold py-5 border border-stone-300 rounded px-3 my-2 hover:bg-slate-300 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-slate-300 duration-150'
+      >
+        <span>Manage Techs</span>
+      </Link>
+      <Link
+        to='/profile/manage-services'
+        className='text-md font-semibold py-5 border border-stone-300 rounded px-3 my-2 hover:bg-slate-300 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-slate-300 duration-150'
+      >
+        <span>Manage Services</span>
+      </Link>
+    </div>
+  );
+}
+
+export default AdminLinks;
