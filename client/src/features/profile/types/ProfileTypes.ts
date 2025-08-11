ea030f238fb3cb196ee544af54cf3487c48a5711
Andrew Smith <meuzishun@gmail.com>
2025-08-11 16:43:36 -0400

    Switch types to one from main

--- client/src/features/profile/types/ProfileTypes.ts
+++ client/src/features/profile/types/ProfileTypes.ts
@@ -1,57 +1,47 @@
-export interface BookingT {
-  booking_id: string;
-  client_name: string;
-  date: string;
-  rating_comment: string;
-  rating_score: number;
-  service_name: string;
-  status: string;
-}
-
-type BookingsT = {
-  past: BookingT[];
-  upcoming: BookingT[];
-};
-
-export interface StatsT {
-  bookings_completed: number;
-  years_on_platform: number;
-}
-
-export interface ProfileT {
-  role: 'admin' | 'technician' | 'customer';
-  id: string;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone: string;
-  rating_score: string;
-  bookings: BookingsT;
-  stats: StatsT;
-}
+// type UserT = {
+//   userType: 'admin' | 'tech' | 'client';
+//   id: string;
+//   first_name: string;
+//   last_name: string;
+//   email: string;
+//   phone: string;
+// };
 // type TechnicianT = {
 //   techId: string;
 //   techName: string;
 //   techRating: number;
 // };
 
-// type BookingT = {
-//   bookingId: string;
-//   serviceId: string;
-//   serviceName: string;
-//   date: string;
-//   time: string;
-//   address: string;
-//   status: string;
-// };
+export type BookingT = {
+  booking_id: string;
+  technician_name: string;
+  client_name: string;
+  service_name: string;
+  status: 'scheduled' | 'in_progress' | 'completed' | 'cancelled';
+  date: string;
+  rating_score: number;
+  rating_comment: string;
+  // address: string;
+};
 
-// export interface UserProfileT {
-//   user: UserT;
-// }
+export interface UserProfileT {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone?: string;
+  role: 'technician' | 'customer';
+  rating_score?: number;
+  bookings: { past: BookingT[]; upcoming: BookingT[] };
+  stats: { bookings_completed: number; years_on_platform: number };
+}
 
 export interface AddressT {
   street: string;
   city: string;
   state: string;
-  zipCode: string;
+  zip: string;
+  id: string;
+  isPrimary: boolean | string;
+  latitude?: number;
+  longitude?: number;
 }
