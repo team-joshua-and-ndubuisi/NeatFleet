93fcbcc457e5298b0f759362cd7939b4b40890d3
Andrew Smith <meuzishun@gmail.com>
2025-08-11 16:28:06 -0400

    Adjust font sizes

--- client/src/features/profile/components/ProfileStats.tsx
+++ client/src/features/profile/components/ProfileStats.tsx
@@ -7,12 +7,12 @@ interface ProfileStatsProps {
 
 const ProfileStats: React.FC<ProfileStatsProps> = ({ stats }) => {
   return (
-    <div className=' flex flex-row items-center justify-around bg-primary-50 border rounded-lg shadow-md p-5'>
-      <span className='py-5 text-2xl font-light text-center'>
+    <div className='flex flex-row items-center justify-around bg-primary-50 border rounded-lg shadow-md p-5'>
+      <span className='py-5 text-lg lg:text-2xl font-light text-center'>
         Number of Bookings: <br />
         <span className='font-bold text-4xl'>{stats.bookings_completed}</span>
       </span>
-      <span className='py-5 text-2xl font-light text-center'>
+      <span className='py-5 text-lg lg:text-2xl font-light text-center'>
         Years at NeatFleet: <br />
         <span className='font-bold text-4xl'>{stats.years_on_platform}</span>
       </span>
