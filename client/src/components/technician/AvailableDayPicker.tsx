9fd30658f7cd45419ae1515118407aaf3b34fc99
jesus doza <jesus.doza@gmail.com>
2025-08-13 18:48:52 -0500

    clean up

--- client/src/components/technician/AvailableDayPicker.tsx
+++ client/src/components/technician/AvailableDayPicker.tsx
@@ -1,4 +1,3 @@
-import { ToggleGroupItem } from '@/components/ui/toggle-group';
 import { Calendar } from '@/components/ui/calendar';
 
 export type DayT = {
@@ -7,16 +6,6 @@ export type DayT = {
   // dayOfWeek: number; // Optional, used for Sunday
   date: Date;
 };
-
-// const daysOfWeek: DayT[] = [
-//   { value: 'sunday', label: 'Sunday', dayOfWeek: 0 },
-//   { value: 'monday', label: 'Monday', dayOfWeek: 1 },
-//   { value: 'tuesday', label: 'Tuesday', dayOfWeek: 2 },
-//   { value: 'wednesday', label: 'Wednesday', dayOfWeek: 3 },
-//   { value: 'thursday', label: 'Thursday', dayOfWeek: 4 },
-//   { value: 'friday', label: 'Friday', dayOfWeek: 5 },
-//   { value: 'saturday', label: 'Saturday', dayOfWeek: 6 },
-// ];
 
 type AvailableDayPickerPropT = {
   clickCallback?: (day: Date) => void;
