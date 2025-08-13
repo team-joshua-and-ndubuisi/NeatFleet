27cbf9b8946d8e012235b7fd38460c9ecf98282a
Yassahr <yassahr@gmail.com>
2025-08-13 15:48:13 -0400

    fix: removed class property from svg

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
           <div className=' md:mb-0'>
             <div className="flex items-center ">
               <Link to='/' >
-                <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-soap-dispenser-droplet h-8 w-8" aria-hidden="true"><path d="M10.5 2v4"></path><path d="M14 2H7a2 2 0 0 0-2 2"></path><path d="M19.29 14.76A6.67 6.67 0 0 1 17 11a6.6 6.6 0 0 1-2.29 3.76c-1.15.92-1.71 2.04-1.71 3.19 0 2.22 1.8 4.05 4 4.05s4-1.83 4-4.05c0-1.16-.57-2.26-1.71-3.19"></path><path d="M9.607 21H6a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h7V7a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3"></path></svg>
+                <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  aria-hidden="true"><path d="M10.5 2v4"></path><path d="M14 2H7a2 2 0 0 0-2 2"></path><path d="M19.29 14.76A6.67 6.67 0 0 1 17 11a6.6 6.6 0 0 1-2.29 3.76c-1.15.92-1.71 2.04-1.71 3.19 0 2.22 1.8 4.05 4 4.05s4-1.83 4-4.05c0-1.16-.57-2.26-1.71-3.19"></path><path d="M9.607 21H6a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h7V7a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3"></path></svg>
                 <span> NeetFleet </span>
               </Link>
 
