2c82293807a789974b7eecd0ebdf7288c89aae1c
jesus doza <jesus.doza@gmail.com>
2025-08-13 23:27:36 -0500

    fix logout not clearing old data

--- client/src/features/auth/hooks/useLogout.ts
+++ client/src/features/auth/hooks/useLogout.ts
@@ -1,12 +1,15 @@
 import { useAuthStore } from '@/features/auth';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
+import { useQueryClient } from '@tanstack/react-query';
 
 export const useLogout = () => {
+  const client = useQueryClient();
   const { setUserToken } = useAuthStore();
   const navigate = useNavigate();
 
   return () => {
+    client.removeQueries({ queryKey: ['profile'] });
     setUserToken('');
     toast.success('Logged out successfully');
     navigate('/login');
